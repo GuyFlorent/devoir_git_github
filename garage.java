4345c6e1a6b185ceeb7a712869b74d442e54c6fd
WANGUEU <wangueujunior23@gmail.com>
2019-10-04 06:17:10 +0200

    la liste des enum Marque

--- garage.java
+++ garage.java
@@ -37,3 +37,25 @@ public abstract class Vehicule implements Serializable{
 		this.mot = mot;
 	}
 }
+
+package voiture;
+
+import java.io.Serializable;
+
+public enum Marque  implements Serializable{
+
+	RENO ("Voiture RENO"),
+	PIGEOT ("Voiture PIGEOT"),
+	TROEN ("Voiture TROEN");
+
+	private String nom = "";
+
+	Marque(String pNom){
+		this.nom = pNom;
+	}
+
+	public String toString(){
+		return this.nom;
+	}
+
+}
